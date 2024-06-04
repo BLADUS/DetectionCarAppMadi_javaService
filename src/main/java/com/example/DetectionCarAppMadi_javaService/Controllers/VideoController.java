e22087475f2f0e897a20a2ee89c7a846408606b2
Vladislav Osada <Osada.0339@gmail.com>
2024-06-05 01:03:15 +0300

    Передача данных пользователя на сервис питона при отправки видефайла

--- src/main/java/com/example/DetectionCarAppMadi_javaService/Controllers/VideoController.java
+++ src/main/java/com/example/DetectionCarAppMadi_javaService/Controllers/VideoController.java
@@ -20,6 +20,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import com.example.DetectionCarAppMadi_javaService.Entity.User;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class VideoController {
     @Value("${flask.url}")
@@ -30,33 +34,41 @@ public class VideoController {
         return "upload-video-form";
     }
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> handleFileUpload(@RequestParam("video") MultipartFile file) {
-        try {
-            RestTemplate restTemplate = new RestTemplate();
-            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(flaskUrl + "/upload_video");
-
-            ByteArrayResource videoResource = new ByteArrayResource(file.getBytes()) {
-                @Override
-                public String getFilename() {
-                    return file.getOriginalFilename();
-                }
-            };
-
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-
-            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-            body.add("video", videoResource);
-            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
-
-            ResponseEntity<String> response = restTemplate.postForEntity(builder.toUriString(), requestEntity,
-                    String.class);
-
-            return new ResponseEntity<>("Video uploaded successfully", HttpStatus.OK);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("Video upload failed", HttpStatus.INTERNAL_SERVER_ERROR);
+   @PostMapping("/upload")
+public ResponseEntity<String> handleFileUpload(@RequestParam("video") MultipartFile file, HttpSession session) {
+    try {
+        User currentUser = (User) session.getAttribute("currentUser");
+        if (currentUser == null) {
+            return new ResponseEntity<>("User not logged in", HttpStatus.UNAUTHORIZED);
         }
+
+        String username = currentUser.getUsername();
+
+        RestTemplate restTemplate = new RestTemplate();
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(flaskUrl + "/upload_video");
+
+        ByteArrayResource videoResource = new ByteArrayResource(file.getBytes()) {
+            @Override
+            public String getFilename() {
+                return file.getOriginalFilename();
+            }
+        };
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("video", videoResource);
+        body.add("username", username);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(builder.toUriString(), requestEntity, String.class);
+
+        return new ResponseEntity<>("Video uploaded successfully", HttpStatus.OK);
+    } catch (IOException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>("Video upload failed", HttpStatus.INTERNAL_SERVER_ERROR);
     }
+}
+
 }
